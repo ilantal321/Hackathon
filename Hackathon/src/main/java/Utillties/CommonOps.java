465f6838abfe915c66bb1999445b1576e0557023
ilan_tal <ilan.tal321@gmail.com>
2021-11-29 09:41:37 +0200

    add web platform

--- Hackathon/src/main/java/Utillties/CommonOps.java
+++ Hackathon/src/main/java/Utillties/CommonOps.java
@@ -6,6 +6,8 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -17,8 +19,18 @@ public class CommonOps extends Base {
 
     @BeforeClass
     public void startSession() {
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+        if (ManageDDT.getData("WebPlatform").equals("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        }
+        if (ManageDDT.getData("WebPlatform").equals("FireFox")) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        }
+        if (ManageDDT.getData("WebPlatform").equals("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            webDriver = new EdgeDriver();
+        }
         webDriver.manage().window().maximize();
         webDriver.get(ManageDDT.getData("URL"));
         ManagerPages.makePOLoginPage();
